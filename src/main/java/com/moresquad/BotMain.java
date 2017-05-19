a165533247b2efbe855977fd701f3bab8e7aa63d
VadimOza <worldofvadim@gmail.com>
2017-05-19 12:08:11 +0300

    Merge

--- src/main/java/com/moresquad/BotMain.java
+++ src/main/java/com/moresquad/BotMain.java
@@ -48,7 +48,7 @@ public class BotMain {
                     session1.sendMessage(event.getChannel(), "Максу слава :pride:");
                 if (event.getSender().getUserName().equals("lendyelushka") && (event.getMessageContent().contains("сука") || event.getMessageContent().contains("уйобок")))
                     session1.sendMessage(event.getChannel(), "Я краще б і не сказав");
-                if (event.getSender().getUserName().equals("lendyelushka") && random.nextInt(100) > 98 )
+                if (event.getSender().getUserName().equals("lendyelushka") && random.nextInt(100) > 98)
                     session1.sendMessage(event.getChannel(), "Я так рідко тобі це кажу, але я люблю тебе МАКС!!!");
                 if (event.getMessageContent().contains("буха"))
                     session1.sendMessage(event.getChannel(), "Хтось сказав бухати???? Я за!! І Макс к стате тоже :beers: :beer: :beers:");
@@ -56,13 +56,13 @@ public class BotMain {
                     session1.sendMessage(event.getChannel(), "Ти щось сказав про мою мамку? :middle_finger::angry:");
 
                 if (event.getMessageContent().contains("обзови ")) {
-					try {
-						if (event.getMessageContent().contains("вадім"){
-							session1.sendMessage(event.getChannel(), "Вадім - мати моя любима. Не можна обзивати його, обізватий будеш ти!");
-							session1.sendMessage(event.getChannel(), scheduleCallSb(event.getSender().getUserName()+":hankey::hankey::hankey:");
-						}  else{
-							session1.sendMessage(event.getChannel(), scheduleCallSb(event.getMessageContent().replace("обзови ", "")));
-						}	
+                    try {
+                        if (event.getMessageContent().contains("вадім")) {
+                            session1.sendMessage(event.getChannel(), "Вадім - мати моя любима. Не можна обзивати його, обізватий будеш ти!");
+                            session1.sendMessage(event.getChannel(), scheduleCallSb(event.getSender().getUserName() + ":hankey::hankey::hankey:"));
+                        } else {
+                            session1.sendMessage(event.getChannel(), scheduleCallSb(event.getMessageContent().replace("обзови ", "")));
+                        }
                     } catch (InterruptedException | ExecutionException | TimeoutException e) {
                         session1.sendMessage(event.getChannel(), "чет не вышло :cry:");
                     }
